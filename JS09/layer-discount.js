fb463288067587bc38861a8679b949a11505f564
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-02-14 23:07:12 +0600

    function basics 08

---
+++ JS09/layer-discount.js
@@ -0,0 +1,27 @@
+function layerDiscount(quantity){
+    const first100Price = 100
+    const second100Price = 90
+    const above200Price = 70
+
+    if(quantity <= 100){
+        const total = quantity * first100Price
+        return total
+    }
+    else if(quantity <= 200){
+        const first100PTotal = 100 * first100Price
+        const remainingQuantity = quantity - 100
+        const remainingTotal = remainingQuantity * second100Price
+        const total = first100PTotal + remainingTotal
+        return total
+    }
+    else{
+        const first100PTotal = 100 * first100Price
+        const second100Total = 100 * second100Price
+        const remainingQuantity = quantity - 200
+        const remainingTotal = remainingQuantity * above200Price
+        const total = first100PTotal + second100Total + remainingTotal
+        return total
+    }
+}
+const product = layerDiscount(10)
+console.log(product)
--- JS09/shopping-cart.js
+++ JS09/shopping-cart.js
@@ -2,19 +2,35 @@ const products = [
   {
     name: "Shampoo",
     price: 300,
+    quantity:2
   },
   {
     name: "chiruni",
     price: 100,
+    quantity:3
   },
   {
     name: "Shirt",
     price: 700,
+    quantity:5
   },
   {
     name: "Pant",
     price: 1200,
+    quantity:1
   },
 ];
+
+function getProductPrice(products){
+    let total = 0
+    for(const product of products){
+        const productCost = product.price * product.quantity
+        total += productCost
+    }
+    return total
+}
+
+const product = getProductPrice(products)
+console.log(product)
 
 
