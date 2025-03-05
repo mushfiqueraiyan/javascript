595900dc7873da8a8ace21a0b708493cf7c07f09
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-03-05 19:05:50 +0600

    more es6

---
+++ ES6/distructuring.js
@@ -0,0 +1,21 @@
+const raiyan = {
+    name: "Raiyan",
+    age: 20,
+    friends: ['Omar', 'Mahfuz', 'Arif'],
+    status: "Ki bolbo"
+}
+
+// const raiyanStatus = raiyan.status;
+// console.log(raiyanStatus);
+
+const {name,age,status,friends} = raiyan;
+console.log(name,age)
+
+
+
+const person = ['Omar', 'Mahfuz', 'Arif'];
+// console.log(person[0])
+
+const [nayok, villain] = person;
+console.log(nayok, villain) //  Haha..
+
---
+++ ES6/spread.js
@@ -0,0 +1,6 @@
+const arr = [12,65,23,56,55,23,1,2,6,8,90,23,100];
+console.log(Math.max(...arr));
+
+const friends = ['Mushfique', 'Raiyan', 'MushfiqueRaiyan']
+const newFriends = ['Mahfuz', 'Omar', 'Arif', ...friends];
+console.log(newFriends)
