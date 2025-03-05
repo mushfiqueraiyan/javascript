6da84ccd5fd7b310c1d7c79a58facdc8b48a00c0
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-03-05 18:49:00 +0600

    ES6 start

---
+++ ES6/arrowFunc.js
@@ -0,0 +1,12 @@
+const arrow = (num1, num2)=>{
+    return num1 + num2;
+}
+
+console.log(arrow(10,20))
+
+
+const arrow2 =x=> x * x
+console.log(arrow2(10));
+
+const arrow3 =(num1, num2)=> num1 - num2;
+console.log(arrow3(10,5))
